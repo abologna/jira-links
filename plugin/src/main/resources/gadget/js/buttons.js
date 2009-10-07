344f87e684dac0014ecdc1b922f1346d7159b696
Pablo Fernandez <fernandezpablo85@gmail.com>
2009-10-07 00:52:34 -0300

    fixed form - list nav

--- plugin/src/main/resources/gadget/js/buttons.js
+++ plugin/src/main/resources/gadget/js/buttons.js
@@ -2,14 +2,26 @@ $("document").ready(function(){
 
   $("#new-link").click(function(){
     $("#new-delicious-link-form").fadeOut(function(){
-      $("#new-link-form").fadeIn("slow");
+			$('#link-list').fadeOut(function(){
+				$("#new-link-form").fadeIn("slow");
+			})
     });
   });
 
   $("#new-delicious-link").click(function(){
     $("#new-link-form").fadeOut(function(){
-      $("#new-delicious-link-form").fadeIn("slow");
+				$('#link-list').fadeOut(function(){
+					$("#new-delicious-link-form").fadeIn("slow");
+			})
     });
   });
+
+	$('input.cancel').click(function(){
+		$("#new-link-form").fadeOut(function(){
+			$("#new-delicious-link-form").fadeOut(function(){
+				$('#link-list').fadeIn('fast')
+			})
+		})
+	})
 
 });
